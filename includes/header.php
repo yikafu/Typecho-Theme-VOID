329da230d1a6583377c84c2cb191394cfa328676
yikafu <f2149804524@gmail.com>
2024-08-26 10:58:29 +0800

    添加自定义nav、格式化

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * header.php
  * 
@@ -10,50 +11,51 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 $setting = $GLOBALS['VOIDSetting'];
 $banner = $setting['defaultBanner'];
-if($this->is('post') || $this->is('page')) {
+if ($this->is('post') || $this->is('page')) {
     $banner = $this->fields->bannerStyle < 2 ? $this->fields->banner : '';
 }
 ?>
 
-<body fontsize="<?php echo Utils::getTextSize($setting); ?>" class="<?php 
-        if($setting['colorScheme'] == 0){
-            echo((isset($_COOKIE['theme_dark']) && $_COOKIE['theme_dark'] == '1') ? 'theme-dark' : '');
-        } elseif ($setting['colorScheme'] == 2) {
-            echo 'theme-dark';
-        }
-        if($setting['macStyleCodeBlock']) {
-            echo ' macStyleCodeBlock';
-        }
-        if ($setting['lineNumbers']) {
-            echo ' line-numbers';
-        }
-        if(Utils::isSerif($setting)) {
-            echo ' serif';
-        }
-        if(Utils::isMobile()) {
-            echo ' mobile';
-        }
-        if(Utils::isIosSafari()) {
-            echo ' ios-safari';
-        }
-        if ($setting['indexStyle'] == 1) { // 强制不显示
-            echo ' single-col';
-        }
-        if ($setting['bluredLazyload']) { // 强制不显示
-            echo ' bluredLazyload';
-        }
-        if(Helper::options()->lazyload == '1') {
-            echo ' lazyload-img';
-        }
-    ?>">
-    
-    <header class="header-mode-<?php echo $setting['headerMode']; ?> <?php if(empty($banner)) echo 'force-dark no-banner'; ?>">
+<body fontsize="<?php echo Utils::getTextSize($setting); ?>" class="<?php
+                                                                    if ($setting['colorScheme'] == 0) {
+                                                                        echo ((isset($_COOKIE['theme_dark']) && $_COOKIE['theme_dark'] == '1') ? 'theme-dark' : '');
+                                                                    } elseif ($setting['colorScheme'] == 2) {
+                                                                        echo 'theme-dark';
+                                                                    }
+                                                                    if ($setting['macStyleCodeBlock']) {
+                                                                        echo ' macStyleCodeBlock';
+                                                                    }
+                                                                    if ($setting['lineNumbers']) {
+                                                                        echo ' line-numbers';
+                                                                    }
+                                                                    if (Utils::isSerif($setting)) {
+                                                                        echo ' serif';
+                                                                    }
+                                                                    if (Utils::isMobile()) {
+                                                                        echo ' mobile';
+                                                                    }
+                                                                    if (Utils::isIosSafari()) {
+                                                                        echo ' ios-safari';
+                                                                    }
+                                                                    if ($setting['indexStyle'] == 1) { // 强制不显示
+                                                                        echo ' single-col';
+                                                                    }
+                                                                    if ($setting['bluredLazyload']) { // 强制不显示
+                                                                        echo ' bluredLazyload';
+                                                                    }
+                                                                    if (Helper::options()->lazyload == '1') {
+                                                                        echo ' lazyload-img';
+                                                                    }
+                                                                    ?>">
+
+    <header class="header-mode-<?php echo $setting['headerMode']; ?> <?php if (empty($banner)) echo 'force-dark no-banner'; ?>">
         <div class="container wider">
             <nav>
                 <a role=button aria-label="展开导航" class="toggle" target="_self" href="javascript:void(0);" onclick="VOID_Ui.toggleNav(this);">
                     <span></span>
                 </a>
-                <a class="brand" href="<?php Utils::index(''); ?>"><?php if($setting['name']) echo $setting['name']; else echo $this->options->title; ?></a>
+                <a class="brand" href="<?php Utils::index(''); ?>"><?php if ($setting['name']) echo $setting['name'];
+                                                                    else echo $this->options->title; ?></a>
                 <a href="<?php Utils::index(''); ?>">首页</a>
                 <?php $this->widget('Widget_Contents_Page_List')->parse('<a href="{permalink}">{title}</a>'); ?>
                 <span class="dropdown">分类
@@ -61,27 +63,33 @@ if($this->is('post') || $this->is('page')) {
                         <?php $this->widget('Widget_Metas_Category_List')->parse('<li><a href="{permalink}">{name}</a></li>'); ?>
                     </ul>
                 </span>
-                <?php if($setting['nav']){ foreach ($setting['nav'] as $listItem): ?>
-                <span class="dropdown"><?php echo $listItem['name']; ?>
-                    <ul>
-                        <?php foreach ($listItem['items'] as $item) {
-                            $target = '_blank';
-                            if(isset($item['target'])) $target = $item['target'];
-                            echo "<li><a target=\"{$target}\" href=\"{$item['link']}\">{$item['title']}</a></li>";
-                        }?>
-                    </ul>
-                </span>
-                <?php endforeach; } ?>
-                <?php if(!Utils::isPluginAvailable('ExSearch')): ?>
+                <?php if ($setting['nav']) {
+                    foreach ($setting['nav'] as $listItem): ?>
+                        <?php if (isset($listItem['items'])) : ?>
+                            <span class="dropdown"><?php echo $listItem['name']; ?>
+                                <ul>
+                                    <?php foreach ($listItem['items'] as $item) {
+                                        $target = '_blank';
+                                        if (isset($item['target'])) $target = $item['target'];
+                                        echo "<li><a target=\"{$target}\" href=\"{$item['link']}\">{$item['title']}</a></li>";
+                                    } ?>
+                                </ul>
+                            </span>
+                        <?php else: ?>
+                            <a href="<?php echo $listItem['link']; ?>" target="_blank"><?php echo $listItem['name']; ?></a>
+                        <?php endif; ?>
+                <?php endforeach;
+                } ?>
+                <?php if (!Utils::isPluginAvailable('ExSearch')): ?>
                     <span class="hidden-xs search-form-desktop">
                         <label for="search">搜索</label>
                         <input onkeydown="VOID.enterSearch(this);" type="text" name="search-content" id="search" required />
                     </span>
                 <?php endif; ?>
-                <a <?php if(Utils::isPluginAvailable('ExSearch')) echo 'class="search-form-input" style="display:flex"'; ?> 
-                    role=button aria-label="展开搜索" id="toggle-mobile-search" target="_self" 
-                    href="javascript:void(0);" 
-                    onclick="<?php if(!Utils::isPluginAvailable('ExSearch')) echo 'VOID_Ui.toggleSearch(this);'; ?>">
+                <a <?php if (Utils::isPluginAvailable('ExSearch')) echo 'class="search-form-input" style="display:flex"'; ?>
+                    role=button aria-label="展开搜索" id="toggle-mobile-search" target="_self"
+                    href="javascript:void(0);"
+                    onclick="<?php if (!Utils::isPluginAvailable('ExSearch')) echo 'VOID_Ui.toggleSearch(this);'; ?>">
                     <i class="voidicon-search"></i>
                 </a>
                 <a target="_self" href="javascript:void(0);" id="toggle-setting" onclick="VOID_Ui.toggleSettingPanel();"><i class="voidicon-cog"></i></a>
@@ -97,6 +105,13 @@ if($this->is('post') || $this->is('page')) {
         <section id="pages" data-title="PAGES">
             <nav>
                 <?php $this->widget('Widget_Contents_Page_List')->parse('<a href="{permalink}">{title}</a>'); ?>
+                <?php if ($setting['nav']) {
+                    foreach ($setting['nav'] as $listItem): ?>
+                        <?php if (isset($listItem['items']) == false) : ?>
+                            <a href="<?php echo $listItem['link']; ?>" target="_blank"><?php echo $listItem['name']; ?></a>
+                        <?php endif; ?>
+                <?php endforeach;
+                } ?>
             </nav>
         </section>
         <section id="categories" data-title="CATEGORIES">
@@ -104,15 +119,19 @@ if($this->is('post') || $this->is('page')) {
                 <?php $this->widget('Widget_Metas_Category_List')->parse('<a href="{permalink}">{name}</a>'); ?>
             </nav>
         </section>
-        <?php if($setting['nav']){ foreach ($setting['nav'] as $listItem): ?>
-        <section data-title="<?php echo $listItem['name']; ?>">
-            <nav>
-                <?php foreach ($listItem['items'] as $item) {
-                    $target = '_blank';
-                    if(isset($item['target'])) $target = $item['target'];
-                    echo "<a target=\"{$target}\" href=\"{$item['link']}\">{$item['title']}</a>";
-                }?>
-            </nav>
-        </section>
-        <?php endforeach;} ?>
+        <?php if ($setting['nav']) {
+            foreach ($setting['nav'] as $listItem): ?>
+                <?php if (isset($listItem['items'])) : ?>
+                    <section data-title="<?php echo $listItem['name']; ?>">
+                        <nav>
+                            <?php foreach ($listItem['items'] as $item) {
+                                $target = '_blank';
+                                if (isset($item['target'])) $target = $item['target'];
+                                echo "<a target=\"{$target}\" href=\"{$item['link']}\">{$item['title']}</a>";
+                            } ?>
+                        </nav>
+                    </section>
+                <?php endif; ?>
+        <?php endforeach;
+        } ?>
     </div>
